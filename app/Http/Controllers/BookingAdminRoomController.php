8b54b3b1d3a730faf3e001c5c613d81191d8ecb7
Nadhiradhit <nadhiradhitya@gmail.com>
2025-05-27 21:34:46 +0700

    feat: dashboard request in admin

---
+++ app/Http/Controllers/BookingAdminRoomController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Booking;
+use App\Models\Room;
+use Illuminate\Http\Request;
+
+class BookingAdminRoomController extends Controller
+{
+    //
+
+    public function index()
+    {
+        $data = Booking::with(['user', 'room'])->get();
+        $laboratories = Room::all();
+        return view('landing.admin.request.room.request-room', compact('data', 'laboratories'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $validated = $request->validate([
+            'status' => 'required|in:approved,rejected',
+        ]);
+
+        $booking = Booking::findOrFail($id);
+        $booking->update($validated);
+
+        return redirect()->back()->with('success', 'Booking status updated successfully');
+    }
+
+
+}
