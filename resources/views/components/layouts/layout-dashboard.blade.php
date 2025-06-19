59c2dce5140673ef8b07d6dd3acd8e560d73dd29
Nadhiradhit <nadhiradhitya@gmail.com>
2025-06-20 00:47:34 +0700

    chore: styling hidden when in mobile

--- resources/views/components/layouts/layout-dashboard.blade.php
+++ resources/views/components/layouts/layout-dashboard.blade.php
@@ -27,7 +27,7 @@
                     </h1>
                 </div>
 
-                <div class="flex items-center w-auto justify-end xs:w-auto hidden md:block">
+                <div class="flex items-center w-auto justify-end xs:w-auto">
                     <svg width="32" height="32" viewBox="0 0 42 42" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-8 h-8 xs:w-9 xs:h-9 sm:w-10 sm:h-10 md:w-11 md:h-11 lg:w-12 lg:h-12">
                         <g clip-path="url(#clip0_257_997)">
                             <path d="M20.6118 20.6117C24.4078 20.6117 27.4824 17.5371 27.4824 13.7411C27.4824 9.94513 24.4078 6.87054 20.6118 6.87054C16.8158 6.87054 13.7412 9.94513 13.7412 13.7411C13.7412 17.5371 16.8158 20.6117 20.6118 20.6117ZM20.6118 24.047C16.0257 24.047 6.87061 26.3487 6.87061 30.9176V34.3529H34.353V30.9176C34.353 26.3487 25.1979 24.047 20.6118 24.047Z" fill="#1B4D71"/>
@@ -38,7 +38,7 @@
                             </clipPath>
                         </defs>
                     </svg>
-                    <div class="text-secondary-900 ml-2">
+                    <div class="text-secondary-900 ml-2 hidden md:block">
                         @auth
                             <h4 class="text-sm xs:text-base sm:text-lg font-semibold truncate max-w-32 xs:max-w-none">{{ auth()->user()->name }}</h4>
                             <p class="text-xs sm:text-sm font-normal">As User</p>
