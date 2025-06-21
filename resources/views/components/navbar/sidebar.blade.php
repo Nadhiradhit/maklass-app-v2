01c990fe32e66d0f2cab90af500b82ebb2ce8813
Nadhiradhit <nadhiradhitya@gmail.com>
2025-06-21 17:42:38 +0700

    chore: change link name

--- resources/views/components/navbar/sidebar.blade.php
+++ resources/views/components/navbar/sidebar.blade.php
@@ -164,7 +164,7 @@
                                             </clipPath>
                                         </defs>
                                     </svg>
-                                    <span>Cari Kelas</span>
+                                    <span>Cari Ruangan Lab</span>
                                 </span>
                                 <svg :class="{ 'transform rotate-180': openDropdown === 'class' }" class="w-4 h-4 transition-transform" fill="currentColor" viewBox="0 0 20 20">
                                     <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path>
